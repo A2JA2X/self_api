438f48b8026629c6f8cbdec0c9003c69ed75df5f
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-29 16:47:05 +0900

    Add dummy response

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ const route = '/api/v1/list';
 const port = 3000;
 
 app.get(route, (req, res) => {
-
+  const data = {
+    dishes: [
+      {
+        name: 'Fish and potatos',
+        price: 800
+      },
+      {
+        name: 'Fish and Tomato',
+        price: 200
+      },
+      {
+        name: 'Steak and potatos',
+        price: 500
+      }
+    ]
+  }
+  res.json(data);
 });
 
 app.listen(port, () => {
