63dd3a4b63564fde6edfebf34ecd18d9e201ff5b
hiroshi_watanabe <h.watanabe2529@gmail.com>
2019-04-29 18:11:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //expressモジュールを読み込み
 const express = require('express');
+// MongoDB用モジュール読み込み
+const mongo = require('./mongo');
 //expressアプリを生成
 const app = express();
 //ルート
@@ -7,24 +9,17 @@ const route = '/api/v1/list';
 //ポート番号
 const port = 3000;
 
+// CORSを許可する
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get(route, (req, res) => {
-  const data = {
-    dishes: [
-      {
-        name: 'Fish and potatos',
-        price: 800
-      },
-      {
-        name: 'Fish and Tomato',
-        price: 200
-      },
-      {
-        name: 'Steak and potatos',
-        price: 500
-      }
-    ]
-  }
-  res.json(data);
+  mongo.collection('item').find().toArray((err, docs) => {
+    res.json(docs);
+  });
 });
 
 app.listen(port, () => {
